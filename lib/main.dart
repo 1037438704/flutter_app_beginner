f12adceb2c13fa1d2bc4d201a1c31d6710ae254d
赵栋亮 <zhaodongliang@itimebox.com>
2019-12-09 18:01:39 +0800

    修改依赖

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_beginner/pages/camera_page.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    requestPermiss();
     return Container(
       child: MaterialApp(
         home: ImagePickerWidget(),
       ),
     );
   }
-}
 
+  requestPermiss() async {
+    //请求权限
+    Map<PermissionGroup, PermissionStatus> permissions =
+    await PermissionHandler()
+        .requestPermissions([PermissionGroup.camera,PermissionGroup.microphone,PermissionGroup.photos,PermissionGroup.storage]);
+    //校验权限
+    if(permissions[PermissionGroup.camera] != PermissionStatus.granted){
+      print("无照相权限");
+    }
+    if(permissions[PermissionGroup.location] != PermissionStatus.granted){
+      print("无定位权限");
+    }
+  }
+}
